17af0afe6889911e14158193878dcab1ac605ad3
Joshua Farin <farin.k.joshua@gmail.com>
2020-03-31 12:00:17 -1000

    Posting data

---
+++ Core Sandbox/Pages/Forms/AddAddress.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Core_Sandbox
+{
+    public class AddAddressModel : PageModel
+    {
+        [BindProperty]
+        public AddAddressModel Address { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid == false)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Index");
+
+        }
+    }
+}
