0fa29669a334de10e3b5542474ba6d5f27885c9a
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-05 08:33:32 -1000

    Complex Database

--- ContosoUniversity/Pages/Students/Delete.cshtml.cs
+++ ContosoUniversity/Pages/Students/Delete.cshtml.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Data;
+
 using ContosoUniversity.Models;
 
 namespace ContosoUniversity
@@ -30,7 +28,7 @@ namespace ContosoUniversity
                 return NotFound();
             }
 
-            Student = await _context.Student
+            Student = await _context.Students
                 .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
                 
