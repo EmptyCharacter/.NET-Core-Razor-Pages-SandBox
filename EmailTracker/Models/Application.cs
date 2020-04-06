492cabb28813c99144357d4558cd72e272646299
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-06 12:36:35 -1000

    Custom attributes(Area, AppStatus )

--- EmailTracker/Models/Application.cs
+++ EmailTracker/Models/Application.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,11 @@ namespace EmailTracker.Models
 {
     public class Application
     {
+        [DataType(DataType.)]
         public DateTime DateSent { get; set; }
         public string EmployerName { get; set; }
         public string Position { get; set; }
+
         public string Area { get; set; }
         public string AppStatus { get; set; }
     }
