41d5def688d4cbbf5c1441710959a9af34ff308b
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-06 14:03:50 -1000

    Model and scaffolding databsae

---
+++ WebApplication1/Models/AppTracker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class AppTracker
+    {
+        public int ID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateSent { get; set; }
+        public string EmployerName { get; set; }
+        public string Position { get; set; }
+        public string Area { get; set; }
+        public string AppStatus { get; set; }
+    }
+}
---
+++ WebApplication1/Pages/AppTracker/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly WebApplication1.Data.WebApplication1Context _context;
+
+        public DeleteModel(WebApplication1.Data.WebApplication1Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public AppTracker AppTracker { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            AppTracker = await _context.AppTracker.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (AppTracker == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            AppTracker = await _context.AppTracker.FindAsync(id);
+
+            if (AppTracker != null)
+            {
+                _context.AppTracker.Remove(AppTracker);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
