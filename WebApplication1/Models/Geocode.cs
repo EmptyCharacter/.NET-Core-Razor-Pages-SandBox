bca869566ef03e8c810d921128cc9477cd705ccb
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-08 16:38:14 -1000

    Hash out logic

--- WebApplication1/Models/Geocode.cs
+++ WebApplication1/Models/Geocode.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GoogleMaps.LocationServices;
 
 namespace WebApplication1.Models
 {
     public class Geocode
     {
+           //first create array populated with area names from db
 
+
+           //then pass that through method to convert from city name to,
+           //lat long format and store that in an array
     }
 }
