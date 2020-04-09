3a97e374b8bdc49745e78a2363891f925d2796fe
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-08 19:28:25 -1000

    Logic for creating list of areas

--- WebApplication1/Models/Geocode.cs
+++ WebApplication1/Models/Geocode.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using GoogleMaps.LocationServices;
+using Microsoft.Data.SqlClient;
 
 namespace WebApplication1.Models
 {
     public class Geocode
     {
-           //first create array populated with area names from db
+        
 
 
-           //then pass that through method to convert from city name to,
-           //lat long format and store that in an array
+        //first create list populated with area names from db
+        public List<string> getFromDataBase()
+        {
+            
+            List<string> list = new List<string>();
+
+            DataTable areas = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.Fill(areas);
+
+            foreach(DataRow row in areas.Rows)
+            {
+                list.Add(row["Area"].ToString());
+            }
+            return list;
+        }
+
+
+        //then pass that through method to convert from city name to,
+        //lat long format and store that in an array
     }
 }
