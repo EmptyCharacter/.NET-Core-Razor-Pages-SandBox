62a7790839ed98d989918cd48843c1cde2957fdb
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-10 19:20:10 -1000

    Seed Db model

---
+++ Application/Migrations/20200411050750_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Application.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EntryInfo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployerName = table.Column<string>(nullable: true),
+                    Position = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Decision = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntryInfo", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntryInfo");
+        }
+    }
+}
---
+++ Application/Models/SeedData.cs
@@ -0,0 +1,78 @@
+﻿using Application.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context = new ApplicationContext(
+                serviceProvider.GetRequiredService<
+                DbContextOptions<ApplicationContext>>()))
+            {
+                if(context.EntryInfo.Any())
+                {
+                    return;
+                }
+
+                context.EntryInfo.AddRange(
+                    new EntryInfo
+                    {
+                        EmployerName = "",
+                        Position = "",
+                        City = "",
+                        Date = DateTime.Parse("2020-4-13"),
+                        Decision = ""
+                    },
+
+                    new EntryInfo
+                    {
+                        EmployerName = "",
+                        Position = "",
+                        City = "",
+                        Date = DateTime.Parse("2020-4-13"),
+                        Decision = ""
+                    },
+
+                    new EntryInfo
+                    {
+                        EmployerName = "",
+                        Position = "",
+                        City = "",
+                        Date = DateTime.Parse("2020-4-13"),
+                        Decision = ""
+                    },
+
+                    new EntryInfo
+                    {
+                        EmployerName = "",
+                        Position = "",
+                        City = "",
+                        Date = DateTime.Parse("2020-4-13"),
+                        Decision = ""
+                    },
+
+                    new EntryInfo
+                    {
+                        EmployerName = "",
+                        Position = "",
+                        City = "",
+                        Date = DateTime.Parse("2020-4-13"),
+                        Decision = ""
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+
+
+
+
+    }
+}
