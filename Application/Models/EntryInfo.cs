dea7993c83cd6ec6ba5b0ada189c3fa8b2b7544c
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-10 18:06:06 -1000

    New Model

---
+++ Application/Models/EntryInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class EntryInfo
+    {
+        public int ID { get; set; }
+        public string EmployerName { get; set; }
+        public string Position { get; set; }
+
+        public string City { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Decision { get; set; }
+
+    }
+}
