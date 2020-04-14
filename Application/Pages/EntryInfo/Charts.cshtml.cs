90cc514aba26b194d3284de362570599de85ffd6
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-13 14:09:28 -1000

    Query db to chart in razor page

---
+++ Application/Pages/EntryInfo/Charts.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebMatrix.Data;
+namespace Application
+{
+    public class ChartsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
