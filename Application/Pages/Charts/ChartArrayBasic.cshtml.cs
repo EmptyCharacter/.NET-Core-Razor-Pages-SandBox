82c513dd251a799f07f4ad28d37545665cae5cb8
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-14 07:28:57 -1000

    Attempt to change nuget packages did not work

--- Application/Pages/Charts/ChartArrayBasic.cshtml.cs
+++ Application/Pages/Charts/ChartArrayBasic.cshtml.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using WebMatrix.Data;
+
 namespace Application
 {
-    public class ChartsModel : PageModel
+    public class ChartArrayBasicModel : PageModel
     {
         public void OnGet()
         {
