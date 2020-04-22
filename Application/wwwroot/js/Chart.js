9a95c57403b88183c4f9d3bde93aa83729aae7ca
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-22 02:15:57 -1000

    logic

--- Application/wwwroot/js/Chart.js
+++ Application/wwwroot/js/Chart.js
@@ -1,32 +1,9 @@
-﻿function LoadChart1() {
-    var data;
-    $.ajax({
-        type: "POST",
-        url: "Analysis.aspx/GetChart1",
-        data: "{account: '" + $("[id*=ddlAccount]").val() + "'}",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (r) {
-            $("#dvChart").html("");
-            $("#dvLegend").html("");
-            var obj = r.d;
-            data = JSON.parse(obj);
+﻿//so first the data has to be sent here either from a controller or something of that nature
 
-            var el = document.createElement('canvas');
-            $("#dvChart")[0].appendChild(el);
 
-            //Fix for IE 8
-            if ($.browser.msie && $.browser.version == "8.0") {
-                G_vmlCanvasManager.initElement(el);
-            }
-            var ctx = el.getContext('2d');
-            ctx.canvas.width = 500;
-            ctx.canvas.height = 500;
-            var userStrengthsChart;
-            userStrengthsChart = new Chart(ctx).Bar(data);
-        },
-        failure: function (response) {
-            alert('There was an error.');
-        }
-    });
-}
+
+//then next we would parse that data in JSON format
+
+
+//Pass that data through to a method that will be understood by the chart js library
+
