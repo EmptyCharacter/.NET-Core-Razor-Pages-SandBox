3940ab19d4129f1842412374e1bf2ef0785d3fc3
Joshua Farin <farin.k.joshua@gmail.com>
2020-04-22 02:28:44 -1000

    Prep questions for next work block

--- Application/wwwroot/js/Chart.js
+++ Application/wwwroot/js/Chart.js
@@ -1,5 +1,5 @@
 ﻿//so first the data has to be sent here either from a controller or something of that nature
-
+//in json format
 
 
 //then next we would parse that data in from json to a javascript object so that 
@@ -7,4 +7,15 @@
 
 
 //Pass that data through to a method that will be understood by the chart js library
+
+
+
+
+//Get Data to display to view w/ third party library
+//---------------------------------------------------------------------------
+//How can I convert the data from my database into JSON format?
+//Once converted how would I pass that data through? From where and to where?
+//What is ajax and what is it used for?
+//In the context of chart js, how can I display multiple charts?
+//How can I make these charts update dynamically?
 
