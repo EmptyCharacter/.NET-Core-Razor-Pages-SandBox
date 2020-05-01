aa525f2db0b86ea453006d9443fd50e63f4a613a
Joshua Farin <farin.k.joshua@gmail.com>
2020-05-01 06:07:51 -1000

    fetching with linq

--- Application/Models/JSONConvert.cs
+++ Application/Models/JSONConvert.cs
@@ -11,25 +11,12 @@ namespace Application.Models
   
     public List<EntryInfo> getData()
     {
-        var con = ConfigurationManager.ConnectionStrings["ApplicationContext-cbca0afc-a7e0-44c5-bb20-34ae8a99148a"].ToString();
-
-        List<EntryInfo> matchingInfo = new List<EntryInfo>();
-        using(SqlConnection myConnection = new SqlConnection(con))
-        {
-            string oString = "Select * from EntryInfo where ";
-            SqlCommand oCmd = new SqlCommand(oString, myConnection);
-            oCmd.Parameters.AddWithValue();
-            myConnection.Open();
-            using(SqlDataReader oReader = oCmd.ExecuteReader())
-            {
-                while(oReader.Read())
-                {
-                   //retrive info here
-                }
-                myConnection.Close();
-            }
-        }
-        return matchingInfo;
+        var entryItems = await _context.EntryInfo
+            .Where(b => b.Enabled)
+            .OrderBy(b => b.Name)
+            .Select(b => new SelectListItem){
+                Value = bool.Id, Text = b.Name})
+        .ToListAsync();
     }
 
 
