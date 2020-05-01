c1e50809d08d3f967a538eeab01a7f29534e3ee6
Joshua Farin <farin.k.joshua@gmail.com>
2020-05-01 06:36:28 -1000

    Added AppContext class

---
+++ Application/Data/AppContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Data
+{
+    public class AppContext : DbContext
+    {
+
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<AppPositions> AppPositions { get; set; }
+        public DbSet<AppCities> AppCities { get; set; }
+        public DbSet<AppEmployerNames> AppEmployerNames { get; set; }
+    }
+}
