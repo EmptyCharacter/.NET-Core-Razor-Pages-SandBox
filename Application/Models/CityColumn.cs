77ddd5f59d5d7b9acde9026461d23d1e37e612ec
Joshua Farin <farin.k.joshua@gmail.com>
2020-05-04 07:21:46 -1000

    fetch data class

---
+++ Application/Models/CityColumn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class CityColumn
+    {
+        
+        public void setArray()
+        {
+            
+
+
+
+        }
+
+
+
+    }
+}
