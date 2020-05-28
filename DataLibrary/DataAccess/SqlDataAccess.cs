ae3e4c40046f70a35e02b9bceab078b016993e6a
Joshua Farin <farin.k.joshua@gmail.com>
2020-05-28 09:40:03 -1000

    Add datalibrary and data access

---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using DataLibrary.Models;
+using System.Data.SqlClient;
+
+namespace DataLibrary.DataAccess
+{
+    public static class SqlDataAccess
+    {
+
+        public static string GetConnectionString(string connectionName = "ApplicationContext-cbca0afc-a7e0-44c5-bb20-34ae8a99148a")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+
+        public static int SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+        }
+
+    }
+}
---
+++ DataLibrary/Models/EntryInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Models
+{
+    public class EntryInfo
+    {
+        public int ID { get; set; }
+
+        public string EmployerName { get; set; }
+        public string Position { get; set; }
+
+        public string City { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Decision { get; set; }
+    }
+}
