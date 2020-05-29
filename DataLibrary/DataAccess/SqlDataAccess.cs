d7f24ce3e6d4b58e3fad141b21c6d63ec4081582
Joshua Farin <farin.k.joshua@gmail.com>
2020-05-29 07:18:14 -1000

    fix null reference exception in conString

--- DataLibrary/DataAccess/SqlDataAccess.cs
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -10,10 +10,10 @@ namespace DataLibrary.DataAccess
 {
     public static class SqlDataAccess
     {
-        public static string GetConnectionString( 
-            string connectionName = "ApplicationContext")
+        public static string GetConnectionString()
         {
-            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+            return "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ApplicationContext-cbca0afc-a7e0-44c5-bb20-34ae8a99148a;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            
         }
 
         public static List<T> LoadData<T>(string sql)
