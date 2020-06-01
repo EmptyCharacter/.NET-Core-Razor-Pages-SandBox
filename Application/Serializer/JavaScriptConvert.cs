e928cc2590532b3b61e16dfb4656255916be2107
Joshua Farin <farin.k.joshua@gmail.com>
2020-06-01 11:05:02 -1000

    JSConvert class

---
+++ Application/Serializer/JavaScriptConvert.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Html;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Serializer
+{
+    public class JavaScriptConvert
+    {
+        public static IHtmlString SerializeObject(object value)
+        {
+            using (var stringWriter = new StringWriter())
+            using (var jsonWriter = new JsonTextWriter(stringWriter))
+            {
+                var serializer = new JsonSerializer
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+
+                jsonWriter = false;
+                serializer.Serialize(jsonWriter, value);
+
+                return new HtmlString(stringWriter.ToString);
+            }
+        }
+
+
+    }
+}
